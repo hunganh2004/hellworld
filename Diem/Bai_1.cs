b57fafe9779687d5c0073bc8e927b92c75ab08a1
Hùng Anh <hunganh2004hy@gmail.com>
2023-12-05 22:36:59 +0700

    Thêm lớp Diem

---
+++ Diem/Bai_1.cs
@@ -0,0 +1,10 @@
+﻿class Diem
+{
+    private double x;
+    private double y;
+    public Diem() => x = y = 0;
+    public Diem(double x, double y) { this.x = x; this.y = y; }
+    public void Nhap(double x, double y) { this.x = x; this.y = y; }
+    public void Hienthi() => Console.WriteLine($"({x},{y})");
+    public void Khoang_cach(Diem x) => Math.Sqrt(Math.Pow(this.x - x.x, 2) + Math.Pow(this.y - x.y, 2));
+}
