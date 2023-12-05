78953c791ce8e96f18086c904ac1f57c9c5ed189
Hùng Anh <hunganh2004hy@gmail.com>
2023-12-05 23:12:38 +0700

    A

--- Diem/Bai_1.cs
+++ Diem/Bai_1.cs
@@ -10,4 +10,5 @@
     public void Nhap(double x, double y) { this.x = x; this.y = y; }
     public void Hienthi() => Console.WriteLine($"({x},{y})");
     public void Khoang_cach(Diem x) => Math.Sqrt(Math.Pow(this.x - x.x, 2) + Math.Pow(this.y - x.y, 2));
+    // I am thinking about you !
 }
