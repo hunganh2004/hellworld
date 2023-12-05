a4fcaf480a69623f13222c827f1f15216df00f75
Hùng Anh <hunganh2004hy@gmail.com>
2023-12-05 23:06:09 +0700

    Test

--- Diem/Bai_1.cs
+++ Diem/Bai_1.cs
@@ -1,13 +1,14 @@
 ﻿class Diem
 {
-     // Tọa độ x, y
+    // Tọa độ x, y
     private double x;
     private double y;
-     // Phương thức khởi tạo:
+    // Phương thức khởi tạo:
     public Diem() => x = y = 0;
     public Diem(double x, double y) { this.x = x; this.y = y; }
-     // Phương thức hoạt động:
+    // Phương thức hoạt động:
     public void Nhap(double x, double y) { this.x = x; this.y = y; }
     public void Hienthi() => Console.WriteLine($"({x},{y})");
     public void Khoang_cach(Diem x) => Math.Sqrt(Math.Pow(this.x - x.x, 2) + Math.Pow(this.y - x.y, 2));
+
 }
